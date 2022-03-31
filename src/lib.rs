c24821cb74d3034ac48429ff48c09905de905d6a
Ross MacArthur <ross@macarthur.io>
2022-03-31 08:53:04 +0200

    Make the tags configurable

--- src/lib.rs
+++ src/lib.rs
@@ -7,9 +7,6 @@ use crate::ast::{Expr, Span};
 pub use crate::data::{List, Map, Value};
 pub use crate::result::{Error, Result};
 
-const BEGIN_BLOCK: &str = "{{";
-const END_BLOCK: &str = "}}";
-
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Template<'t> {
     tmpl: &'t str,
@@ -22,33 +19,42 @@ struct Sub<'t> {
     expr: Expr<'t>,
 }
 
+pub struct Options<'a> {
+    begin_tag: &'a str,
+    end_tag: &'a str,
+}
+
 /// Compile a new template.
 pub fn template(tmpl: &str) -> Result<Template<'_>> {
     Template::new(tmpl)
 }
 
 impl<'t> Template<'t> {
-    fn new(tmpl: &'t str) -> Result<Self> {
+    pub fn new(tmpl: &'t str) -> Result<Self> {
+        Template::with_options(tmpl, Options::default())
+    }
+
+    pub fn with_options(tmpl: &'t str, opts: Options<'_>) -> Result<Self> {
         let mut cursor = 0;
         let mut subs = Vec::new();
 
         loop {
-            let (i, m) = match tmpl[cursor..].find(BEGIN_BLOCK) {
-                Some(m) => (m, m + BEGIN_BLOCK.len()),
+            let (i, m) = match tmpl[cursor..].find(opts.begin_tag) {
+                Some(m) => (m, m + opts.begin_tag.len()),
                 None => {
-                    if let Some(n) = tmpl[cursor..].find(END_BLOCK) {
-                        let span = Span::new(n, n + END_BLOCK.len());
-                        return Err(Error::new("unexpected end block", tmpl, span));
+                    if let Some(n) = tmpl[cursor..].find(opts.end_tag) {
+                        let span = Span::new(n, n + opts.end_tag.len());
+                        return Err(Error::new("unexpected end tag", tmpl, span));
                     }
                     return Ok(Template { tmpl, subs });
                 }
             };
 
-            let (j, n) = match tmpl[m..].find(END_BLOCK) {
-                Some(n) => (m + n + BEGIN_BLOCK.len(), m + n),
+            let (j, n) = match tmpl[m..].find(opts.end_tag) {
+                Some(n) => (m + n + opts.end_tag.len(), m + n),
                 None => {
                     let span = Span::new(i, m);
-                    return Err(Error::new("unclosed block", tmpl, span));
+                    return Err(Error::new("unclosed tag", tmpl, span));
                 }
             };
 
@@ -62,6 +68,27 @@ impl<'t> Template<'t> {
     }
 }
 
+impl Default for Options<'_> {
+    fn default() -> Self {
+        Self {
+            begin_tag: "{{",
+            end_tag: "}}",
+        }
+    }
+}
+
+impl<'a> Options<'a> {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn tags(mut self, begin: &'a str, end: &'a str) -> Self {
+        self.begin_tag = begin;
+        self.end_tag = end;
+        self
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -69,15 +96,15 @@ mod tests {
     use pretty_assertions::assert_eq;
 
     #[test]
-    fn template_no_tags() {
-        let t = template("").unwrap();
+    fn template_new_no_tags() {
+        let t = Template::new("").unwrap();
         let exp = Template {
             tmpl: "",
             subs: Vec::new(),
         };
         assert_eq!(t, exp);
 
-        let t = template("just testing").unwrap();
+        let t = Template::new("just testing").unwrap();
         let exp = Template {
             tmpl: "just testing",
             subs: Vec::new(),
@@ -86,27 +113,66 @@ mod tests {
     }
 
     #[test]
-    fn template_unclosed_block() {
+    fn template_default_tags() {
+        let t = Template::new("test {{ basic }} test").unwrap();
+        let exp = Template {
+            tmpl: "test {{ basic }} test",
+            subs: vec![Sub {
+                span: Span::new(5, 16),
+                expr: Expr::Value(ast::Value {
+                    span: Span::new(8, 13),
+                    path: vec![ast::Ident {
+                        span: Span::new(8, 13),
+                        ident: "basic",
+                    }],
+                }),
+            }],
+        };
+        assert_eq!(t, exp);
+    }
+
+    #[test]
+    fn template_custom_tags() {
+        let t =
+            Template::with_options("test <basic/> test", Options::new().tags("<", "/>")).unwrap();
+        let exp = Template {
+            tmpl: "test <basic/> test",
+            subs: vec![Sub {
+                span: Span::new(5, 13),
+                expr: Expr::Value(ast::Value {
+                    span: Span::new(6, 11),
+                    path: vec![ast::Ident {
+                        span: Span::new(6, 11),
+                        ident: "basic",
+                    }],
+                }),
+            }],
+        };
+        assert_eq!(t, exp);
+    }
+
+    #[test]
+    fn template_unclosed_tag() {
         let err = template("test {{ test").unwrap_err();
         assert_eq!(
             format!("{:#}", err),
             "
    |
  1 | test {{ test
-   |      ^^ unclosed block
+   |      ^^ unclosed tag
 "
         )
     }
 
     #[test]
-    fn template_unexpected_end_block() {
+    fn template_unexpected_end_tag() {
         let err = template("test }} test").unwrap_err();
         assert_eq!(
             format!("{:#}", err),
             "
    |
  1 | test }} test
-   |      ^^ unexpected end block
+   |      ^^ unexpected end tag
 "
         )
     }
