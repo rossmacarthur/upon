630f0f9834d1c9faf53ef602cc51be52b14531a5
Ross MacArthur <ross@macarthur.io>
2022-07-03 20:04:17 +0200

    Derive `PartialEq` and remove `Eq` bound on `Value`

--- src/value/mod.rs
+++ src/value/mod.rs
@@ -5,13 +5,12 @@ mod from;
 mod ser;
 
 use std::collections::BTreeMap;
-use std::mem;
 
 pub(crate) use crate::value::cow::ValueCow;
 pub use crate::value::ser::to_value;
 
 /// Data to be rendered represented as a recursive enum.
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub enum Value {
     None,
     Bool(bool),
@@ -27,19 +26,3 @@ impl Default for Value {
         Self::None
     }
 }
-
-impl PartialEq for Value {
-    fn eq(&self, other: &Self) -> bool {
-        match (self, other) {
-            (Self::Bool(s), Self::Bool(o)) => s == o,
-            (Self::Integer(s), Self::Integer(o)) => s == o,
-            (Self::Float(s), Self::Float(o)) => s == o,
-            (Self::String(s), Self::String(o)) => s == o,
-            (Self::List(s), Self::List(o)) => s == o,
-            (Self::Map(s), Self::Map(o)) => s == o,
-            _ => mem::discriminant(self) == mem::discriminant(other),
-        }
-    }
-}
-
-impl Eq for Value {}
